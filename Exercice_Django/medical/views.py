87d05448ae64a6e45fed193cd1ad97d0a11e4e9b
Malik Khetim <malik.khetim@asendit.com>
2026-02-05 15:50:37 +0100

    back-end v1

--- Exercice_Django/medical/views.py
+++ Exercice_Django/medical/views.py
@@ -2,6 +2,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.viewsets import ModelViewSet
 from .utils import PrescriptionPagination
+from rest_framework import status
+from rest_framework.response import Response
 
 from .models import Patient, Medication, Prescription
 from .filters import PatientFilter, MedicationFilter
@@ -40,13 +42,21 @@ class PrescriptionViewSet(ModelViewSet):
         "end_date": ["lte"],
     }
 
+    def destroy(self, request, *args, **kwargs):
+        return Response(
+            {"detail": "Suppression interdite"},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED
+        )
+
     """COMM_MKH
     Choix du ModelViewSet :
     L'héritage depuis cette classe permet à notre modèle d'avoir automatiquement :
-        - GET /prescriptions/ → liste (avec filtres)
-        - GET /prescriptions/{id}/ → détail
-        - POST /prescriptions/ → création
-        - PUT /prescriptions/{id}/ → mise à jour complète
-        - PATCH /prescriptions/{id}/ → mise à jour partielle
-        - DELETE /prescriptions/{id}/ → suppression
+        - GET /prescriptions/
+        - GET /prescriptions/{id}/
+        - POST /prescriptions/
+        - PUT /prescriptions/{id}/
+        - PATCH /prescriptions/{id}/
+
+        - DELETE /prescriptions/{id}/
+          (désactivée via une surcharge de destroy étant donné que ça ne fait pas partie du scope de l'exercice)
     """
